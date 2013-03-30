6b36e082b1e52feba67d2157d691e2c76bb48984
Kirill Maksimov <netoneko@gmail.com>
2013-03-30 21:44:22 +0600

    More trusu staff

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ http://docs.vagrantup.com/v2/getting-started
 
 Install Vagrant `apt-get install vagrant`
 
-Add new Vagrant box `vagrant box add precise32  http://files.vagrantup.com/precise32.box`
+Add new Vagrant box `vagrant box add precise64  http://files.vagrantup.com/precise64.box`
 
 Boot your new box `vagrant up`
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,23 @@
+apt-get update # this is important
+
 apt-get install -y  python-software-properties
-add-apt-repository -y ppa:juju/pkgs
+add-apt-repository -y ppa:juju/pkgs # juju repo
+
+# 10 gen MongoDB
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 # 10 gen public key
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list
 
 apt-get update 
 
 #&& apt-get upgrade # haha
 
 apt-get install -y juju apt-cacher-ng libvirt-bin lxc zookeeper # juju 
+apt-get install -y mongodb-10gen # mongodb
+apt-get install -y git beanstalkd curl # tsuru
 
 export AS_USER="sudo su vagrant -c"
+
+$AS_USER "git clone git://github.com/globocom/charms.git /home/vagrant/charms" # tsuru charms
 
 $AS_USER "mkdir -p /home/vagrant/.juju"
 
@@ -18,6 +28,7 @@ environments:
     data-dir: /home/vagrant/tsuru
     admin-secret: b3a5dee4fb8c4fc9a4db04751e5936f4
     default-series: precise
+    charms: /home/vagrant/charms
 ' > /home/vagrant/.juju/environments.yaml"
 
 $AS_USER "mkdir -p /home/vagrant/tsuru"
@@ -28,3 +39,24 @@ if [ ! -d "/home/vagrant/.ssh/id_rsa" ]; then
 fi
 
 $AS_USER "juju bootstrap"
+
+
+echo "### TSURU ###"
+
+curl -sL https://s3.amazonaws.com/tsuru/dist-server/tsuru-collector.tar.gz | sudo tar -xz -C /usr/bin
+
+curl -sL https://s3.amazonaws.com/tsuru/dist-server/tsuru-api.tar.gz | sudo tar -xz -C /usr/bin
+
+mkdir /etc/tsuru
+curl -sL https://raw.github.com/globocom/tsuru/master/etc/tsuru.conf -o /etc/tsuru/tsuru.conf
+
+# tsuru gandalf
+
+curl -sL https://s3.amazonaws.com/tsuru/dist-server/gandalf-bin.tar.gz | sudo tar -xz -C /usr/bin
+curl -sL https://s3.amazonaws.com/tsuru/dist-server/gandalf-webserver.tar.gz | sudo tar -xz -C /usr/bin
+
+curl -sL https://raw.github.com/globocom/gandalf/master/etc/gandalf.conf -o /etc/gandalf.conf
+
+webserver &
+
+git daemon --base-path=/var/repositories --syslog --export-all &
